d82fff31df3fc69aa2f5c0712c4cf7377b8827f7
elitefootwear <cristianabelmachado@gmail.com>
2026-09-10 14:06:10 -0300

    Update app_pedidos.py

--- app_pedidos.py
+++ app_pedidos.py
@@ -1,2 +1,5 @@
+# ---------------------------------------------------------
+# DATOS DEL RESTAURANTE
+# ---------------------------------------------------------
 NOMBRE_LOCAL = "Pizzería Don Pepe"
-TELEFONO_WHATSAPP = "595981123456"  # Su número de WhatsApp
+TELEFONO_WHATSAPP = "595975410239"  # Tu número actualizado
